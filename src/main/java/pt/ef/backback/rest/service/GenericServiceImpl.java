75e31e430ed10b0cace01082a7c4fd0c9f729eb8
João Salavisa <joao.salavisa@gmail.com>
2016-05-26 23:47:18 +0100

    log4j dependency

--- src/main/java/pt/ef/backback/rest/service/GenericServiceImpl.java
+++ src/main/java/pt/ef/backback/rest/service/GenericServiceImpl.java
@@ -1,15 +1,20 @@
 package pt.ef.backback.rest.service;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author jsalavisa
  */
 public class GenericServiceImpl implements GenericService {
 
+	final static Logger logger = Logger.getLogger(GenericServiceImpl.class);
+	
 	/**
 	 * (non-Javadoc)
 	 * @see pt.ef.backback.rest.service.GenericService#getGenericMethod()
 	 */
 	public long getGenericMethod() {
+		logger.info("GenericServiceImpl.getGenericMethod invoked");
 		return 0;
 	}
 
--- src/test/java/pt/ef/backback/rest/service/GenericTest.java
+++ src/test/java/pt/ef/backback/rest/service/GenericTest.java
@@ -1,15 +1,12 @@
 package pt.ef.backback.rest.service;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import pt.ef.backback.rest.service.GenericService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -21,6 +18,5 @@ public class GenericTest extends AbstractJUnit4SpringContextTests {
 	@Test
 	public void testGetGenericMethod() {
 		Assert.assertEquals(0, genericService.getGenericMethod());
-
 	}
 }
