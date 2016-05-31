147f34641899dc3c96904ced5ea3fe9afe04c0fa
João Salavisa <joao.salavisa@gmail.com>
2016-05-31 03:16:48 +0100

    organize imports

--- src/main/java/pt/ef/backbase/rest/support/SecurityController.java
+++ src/main/java/pt/ef/backbase/rest/support/SecurityController.java
@@ -2,7 +2,6 @@ package pt.ef.backbase.rest.support;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
